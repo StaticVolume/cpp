a6a7440711c99cf711cdbd65687c42fb3cf85fae
Yamy <zodeck@mail.ru>
2022-04-18 10:08:43 +1000

    Create MySnake project

---
+++ MyBigBlackSnake/source/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ncurses.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+
+int main(void){
+	int x,y;
+	cin >> y >> x;
+	char v = '*';
+	cout<<"Hello, Word"<<endl;
+	initscr();
+	move(y,x);
+	addch(v);
+	refresh();
+	getch();
+	endwin();
+	
+return 0;
+}
