1be872fed9354c03e84e7f174c2b8bd32516cfc6
Yamy <zodeck@mail.ru>
2022-06-13 14:33:05 +1000

    update hyperthreading

--- quarter_one/last_lesson/last_lesson/main.cpp
+++ quarter_one/last_lesson/last_lesson/main.cpp
@@ -12,8 +12,8 @@
 //                            ***Работа по крестикам и ноликам ****
 
 /*   Очень много сил ушло на работу с графикой, но все же не всё удалось реализовать из-за малого колличества времени, например хотелось бы целиком отрисовку обьектов вывести в отдельный поток,
- *   но, к сожалению, не хватает времени на разбор, из-за этого на завершающем запросе нажатия клавиши ENTER фризится изображение, потому что в ожидании нажатия клавиши приостанавливается поток выполнения и
- *   и появляются не приятные глазу артефакты из-за отсутвия обновления экрана(перерисовки). Все же очень мало времени дается на реализацию, вот если бы дней 15... Эх.
+ *   но, к сожалению, не хватает времени на разбор, из-за этого на завершающем запросе нажатия клавиши ENTER фризится изображение(ИСПРАВЛЕНО!!!!), потому что в ожидании нажатия клавиши приостанавливается поток выполнения и
+ *   и появляются не приятные глазу артефакты из-за отсутвия обновления экрана(перерисовки)(ВНИМАНИЕ ОШИБКА ИСПРАВЛЕНА, НЕОБХОЖИМЫЕ ДАННЫЕ ВЫВЕДЕНЫ В ПОТОК. Все же очень мало времени дается на реализацию, вот если бы дней 15... Эх.
  *   По той же причине могу не успеть сделать адекватный ии. Очень хотелось не использовать тупой алгоритм блокировки, и я постараюсь до такого не опускаться, будет попытка реализовать ии через алгоритм
  *   поиска ближайшего соседа, но на крайний случай буду реализовавать рандом.
  *   Так же отмечу, что большую часть наработок я не удалю, а закоментирую, так как планирую работать дальше в данном направлении, так что закоментированный код оставлен целенаправленно*
@@ -33,7 +33,8 @@
                                                            * Поддержка звуков обработки событий кнопок
                                                            * Поддержка обработки звуков событий ИИ
                                                        * Базовая многопоточность (на данный момент в основном многопоточная инициализация, хотя присутствует многопоточный код иных функций)
-                                                       * Присутствует основа для создания более гибкого приложения, но понадобится рефакторинг, так как из-за спешки некоторые моменты пришлось програмировать в лоб*/
+                                                       * Присутствует основа для создания более гибкого приложения, но понадобится рефакторинг некоторых мест, так как из-за спешки некоторые моменты пришлось програмировать в лоб
+                                                         из-за чего используется много лишних переменных, и лобовой просчёт результата*/
 
 void InitFunc(SDL2* sdl);
 void MusicFunc(SDL2* sdl);
@@ -43,12 +44,13 @@ void GetHumanSelect(SDL2* sdl, Game* game);
 void HumanGetAChoise(SDL2* sdl, Game* game);
 void IiGetAChoise(SDL2* sdl, Game* game);
 
-//void Rend(SDL2* sdl, Game* game, Colour &color);
+void CalcWin(Game* game,SDL_Event &event, bool &SDL_IS_RUN, bool &say_final, bool &cout);
 
 int main()
 {
     bool SDL_IS_RUN = true;
-
+    bool say_final = false;
+    bool cout = true;
     SDL2* sdl2 = new SDL2();
     Game* game = new Game();
     Colour color(0,0,0,0);
@@ -76,12 +78,10 @@ int main()
         SDL_RenderClear(sdl2->GetSDL2_Render());
         sdl2->SDL2DrawLines(color);
 
-        if (game->CalculateWinner() && (event.type != SDL_QUIT)){
-          std::cout << std::endl << "Thank you, Game is Over, Please Enter ENTER_KE to EXIT" << std::endl;
-          std::cin.get();
-          SDL_IS_RUN = false;
-          event.type = SDL_QUIT;
-        }
+            std::thread thread10 (CalcWin, game, std::ref(event), std::ref(SDL_IS_RUN), std::ref(say_final), std::ref(cout));
+            thread10.detach();
+            thread10.~thread();
+
 
 
 
@@ -95,25 +95,29 @@ int main()
 
                      std::thread thread4 (GetHumanSelect,sdl2, game);
                      thread4.detach();
-                     //thread4.~thread();
+                     thread4.~thread();
                    // GetHumanSelect(sdl2, game);
 
      } else if(event.type == SDL_MOUSEBUTTONUP &&  (game->GetChoise() == game->GetHumanChoise())) {
 
-                    /*std::thread thread5 (HumanGetAChoise,sdl2, game);
-                    thread5.detach();
-                    //thread5.~thread();*/
-                    HumanGetAChoise(sdl2, game);
+                    std::thread thread5 (HumanGetAChoise,sdl2, game);
+                    thread5.join();
+                    thread5.~thread();
+                   // HumanGetAChoise(sdl2, game);
             }
-    }
+   }
 
-         /* std::thread thread6(IiGetAChoise,sdl2, game);
-          thread6.detach();
-         thread6.~thread();*/
-         IiGetAChoise(sdl2, game);
+         std::thread thread6(IiGetAChoise,sdl2, game);
+          thread6.join();
+          thread6.~thread();
+        // IiGetAChoise(sdl2, game);
+
+
 
         sdl2->SDL2DrawGameField(game,game->GetSizeOfGameField());
         sdl2->SDL2UpdateWindowSRender();
+
+
 
      }
 
@@ -123,16 +127,26 @@ int main()
 }
 
 
-/*void Rend(SDL2* sdl, Game* game, Colour &color){
+void CalcWin(Game* game, SDL_Event &event, bool &SDL_IS_RUN, bool &say_final, bool & cout){
 
-    SDL_SetRenderDrawColor(sdl->GetSDL2_Render(), 255, 255, 255, 0);
-    SDL_RenderClear(sdl->GetSDL2_Render());
-    sdl->SDL2DrawLines(color);
+   if (!say_final && (event.type != SDL_QUIT )){
 
-    sdl->SDL2DrawGameField(game,game->GetSizeOfGameField());
-    sdl->SDL2UpdateWindowSRender();
+                say_final = game->CalculateWinner();
 
-}*/
+    }else if (say_final) {
+       if (cout){
+       std::cout << std::endl << "Thank you, Game is Over, Please Enter ENTER_KE to EXIT" << std::endl;
+       cout = false;
+       }
+       std::cin.get();
+       SDL_IS_RUN = false;
+       event.type = SDL_QUIT;
+
+   }
+
+ }
+
+
 
 void IiGetAChoise(SDL2* sdl, Game* game) {
 
